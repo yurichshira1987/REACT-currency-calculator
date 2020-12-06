f298e92570a8d5bf8d7fbc01c13a70f167993bb4
unknown <yurichshira1987@gmail.com>
2020-12-06 15:58:04 +0300

    change input.js

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import './input.scss'
+import React from "react";
+import "./input.scss";
 
-function isInvalid({valid,touched,shouldValidate}){
-    return !valid && touched && shouldValidate;
+function isInvalid({ valid, touched, shouldValidate }) {
+  return !valid && touched && shouldValidate;
 }
 
-export const Input=(props)=>{
-    const cls=['modalInput']
-    const inputType=props.type || 'text';
-    const htmlFor=`${props.type}-${Math.random()}`
-    return(
-        <div className={cls.join(' ')}>
-            <label htmlFor={htmlFor}>{props.label}</label>
-            <input type={inputType} id={htmlFor} value={props.value} onChange={props.onChange}  />
-            {isInvalid(props)? <span>{props.errorMessage || 'Введите верное значение'}</span>:null}
-        </div>
-    )
-}
+export const Input = (props) => {
+  const cls = ["modalInput"];
+  const inputType = props.type || "text";
+  const htmlFor = `${props.type}-${Math.random()}`;
+  return (
+    <div className={cls.join(" ")}>
+      <label htmlFor={htmlFor}>{props.label}</label>
+      <input
+        type={inputType}
+        id={htmlFor}
+        value={props.value}
+        onChange={props.onChange}
+      />
+      {isInvalid(props) ? (
+        <span>{props.errorMessage || "Введите верное значение"}</span>
+      ) : null}
+    </div>
+  );
+};
